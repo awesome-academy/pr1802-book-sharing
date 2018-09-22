659bae21cd57916e5761ab5d202987be59d37ef5
manhcuongdtbk <manhcuongdtbk@gmail.com>
2018-09-22 18:40:11 +0700

    Add delegate

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
     foreign_key: "followed_id", dependent:   :destroy
   has_many :following, through: :active_relationships,  source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true, length: {maximum: 255},
     format: {with: VALID_EMAIL_REGEX},
